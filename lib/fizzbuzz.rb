9372b691d73c03f3977effb551373f4cc99e82ac
BlueVajra <cory.leistikow@gmail.com>
2014-03-10 15:58:07 -0600

    Fizz, Buzz and FizzBuzz properly inserting into array

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,11 +2,16 @@ class FizzBuzz
   def initialize(number)
     @number = number
   end
+
   def arrayinator
     the_array = (1..@number).to_a
     the_array.map! do |value|
-      if value % 3 == 0
+      if value % 15 == 0
+        "FizzBuzz"
+      elsif value % 3 == 0
         "Fizz"
+      elsif value % 5 == 0
+        "Buzz"
       else
         value
       end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,15 @@
 require 'fizzbuzz'
 
 describe FizzBuzz do
-  it "prints out an array with fizz replacement" do
+  it "prints out an array with fizz, buzz, and fizzbuzz replacement" do
 
-    fizzbuzz = FizzBuzz.new(5)
+    fizzbuzz = FizzBuzz.new(15)
 
-    expected = [1, 2, "Fizz", 4, 5]
+    expected = [
+      1, 2, "Fizz", 4, "Buzz",
+      "Fizz", 7, 8, "Fizz", "Buzz",
+      11, "Fizz", 13, 14, "FizzBuzz"
+    ]
     actual = fizzbuzz.arrayinator
 
     expect(actual).to eq expected
