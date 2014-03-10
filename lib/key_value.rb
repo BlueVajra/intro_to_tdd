309892661a6c3c203a43a999d26d0669fbdd43a1
BlueVajra <cory.leistikow@gmail.com>
2014-03-10 16:34:09 -0600

    setting and getting key/values

--- lib/key_value.rb
+++ lib/key_value.rb
@@ -4,8 +4,12 @@ class KeyValue
     @store = {}
   end
 
-  def addInfo(key, value)
+  def add_info(key, value)
     @store[key] = value
+  end
+
+  def get_info(key)
+    @store[key]
   end
 
 end
--- spec/key_value_spec.rb
+++ spec/key_value_spec.rb
@@ -1,14 +1,24 @@
 require 'key_value'
 
 describe KeyValue do
-  it "allows the user to give a key and value" do
+  it "allows the user to add key/value and receive value" do
 
     add_value = KeyValue.new
-    add_value.addInfo("dessert", "apple pie")
-    add_value.addInfo("favorite number", 5 )
-    add_value.addInfo(123, "Sesame Street" )
+    add_value.add_info("dessert", "apple pie")
+    add_value.add_info("favorite number", 5 )
+    add_value.add_info(123, "Sesame Street" )
 
-  end
+    expected = "apple pie"
+    actual = add_value.get_info("dessert")
+    expect(actual).to eq expected
+
+    expected = 5
+    actual = add_value.get_info("favorite number")
+    expect(actual).to eq expected
+
+    end
+
+
 
 
 end
