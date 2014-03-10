df96284fe2364366a30abc7c31e1f8bb600d1c13
BlueVajra <cory.leistikow@gmail.com>
2014-03-10 15:49:31 -0600

    Fizz replacement success

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,6 +3,13 @@ class FizzBuzz
     @number = number
   end
   def arrayinator
-    (1..@number).to_a
+    the_array = (1..@number).to_a
+    the_array.map! do |value|
+      if value % 3 == 0
+        "Fizz"
+      else
+        value
+      end
+    end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,14 +1,14 @@
 require 'fizzbuzz'
 
 describe FizzBuzz do
-  it "prints out an array from 1 to a given number" do
+  it "prints out an array with fizz replacement" do
 
     fizzbuzz = FizzBuzz.new(5)
 
-    expected = [1, 2, 3, 4, 5]
+    expected = [1, 2, "Fizz", 4, 5]
     actual = fizzbuzz.arrayinator
 
-    expect(expected).to eq actual
+    expect(actual).to eq expected
 
   end
 
