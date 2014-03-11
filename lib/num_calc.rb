f5a0ebdd27a644ae5cae99fd015a6b067563e8e7
BlueVajra <cory.leistikow@gmail.com>
2014-03-11 14:34:02 -0600

    method subtract takes 2 numbers and returns difference

--- lib/num_calc.rb
+++ lib/num_calc.rb
@@ -2,4 +2,7 @@ class NumCalc
   def add(num1, num2)
     num1 + num2
   end
+  def subtract(num1, num2)
+    num1 - num2
+  end
 end
--- spec/num_calc_spec.rb
+++ spec/num_calc_spec.rb
@@ -1,11 +1,20 @@
 require 'num_calc'
 describe NumCalc do
-  it "takes 2 numbers and adds them" do
+  it "takes 2 numbers and returns their sum" do
 
     numcalc = NumCalc.new
+
     actual = numcalc.add(4,6)
     expected = 10
-
     expect(actual).to eq expected
+  end
+  it "takes 2 numbers and returns their difference" do
+
+    numcalc = NumCalc.new
+
+    actual = numcalc.subtract(10,6)
+    expected = 4
+    expect(actual).to eq expected
+
   end
 end
