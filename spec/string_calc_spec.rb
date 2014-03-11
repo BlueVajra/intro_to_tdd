2a8ca52ba7908c6147667cd11330f085e6c316cd
BlueVajra <cory.leistikow@gmail.com>
2014-03-11 13:52:46 -0600

    method takes a string and returns sum of numbers in string

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,13 +1,12 @@
 require 'string_calc'
 
 describe StringCalc do
-  it "method takes a string and returns a number" do
+  it "method takes a string and returns a sum of the numbers in the string" do
 
     stringcalc = StringCalc.new
 
     actual = stringcalc.add('1,2,7')
     expected = 10
-
     expect(actual).to eq expected
 
   end
